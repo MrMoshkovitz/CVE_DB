39f39a137c96c02ff12e4be98a131ee75babb94b
MrMoshkovitz <gal.moshko@gmail.com>
2025-01-02 21:01:48 +0200

    Enhance update_cves.sh for improved MongoDB container management and error handling
    - Refined logic to check for the existence and running status of the MongoDB container, providing clearer feedback during execution.
    - Added error handling for starting the MongoDB container and pulling the latest image, ensuring robust setup.
    - Improved file handling by checking for the latest CVE data file and falling back to an example file if necessary, enhancing reliability in data imports.
    - Streamlined output messages for better user experience and clarity during script execution.

--- vuln-db-backend/app/update_cves.sh
+++ vuln-db-backend/app/update_cves.sh
@@ -1,5 +1,5 @@
-sudo docker ps -a --filter "name=mongodb" --format "{{.Status}}"
-sudo docker ps --filter "name=mongodb" --filter "status=running"
+# sudo docker ps -a --filter "name=mongodb" --format "{{.Status}}"
+# sudo docker ps --filter "name=mongodb" --filter "status=running"
 
 
 
@@ -14,47 +14,80 @@ sudo chown -R $USER:$USER ~/Code/CVE_DB/vuln-db-backend/data/db
 sudo chmod -R 755 ~/Code/CVE_DB/vuln-db-backend/data/db
 
 
-
-
-# Check if MongoDB container exists but is not running
-# sudo docker ps -a --filter "name=mongodb" --filter "status=exited" | grep mongodb
-# sudo docker ps -a --filter "name=mongodb" --filter "status=running" | grep mongodb
-
-if [ -n "$(sudo docker ps -a --filter "name=mongodb" --filter "status=exited" --format '{{.Names}}')" ]; then
-    echo "MongoDB container exists but is not running. Starting it..."
-    sudo docker start mongodb
-fi
-
-# If its not exists, create it
-if [ -z "$(sudo docker ps -a --filter "name=mongodb" --format '{{.Names}}')" ]; then
-    echo "Creating new MongoDB container..."
-    sudo docker pull mongo:latest
+# Handle MongoDB container - if it doesn't exist, create it, if it exists but is not running, start it, if it exists and is running, do nothing
+#* Check if MongoDB container exists 
+echo -e "\e[1;34mChecking if MongoDB container exists...\e[0m"
+if [ -n "$(sudo docker ps -a --filter "name=mongodb" --format '{{.Names}}')" ]; then
+    echo -e "\e[1;32mSuccess: MongoDB container exists\e[0m"
+    
+    #* Check if MongoDB container is running
+    echo -e "\e[1;34mChecking if MongoDB container is running...\e[0m"
+    if [ -n "$(sudo docker ps --filter "name=mongodb" --filter "status=running" --format '{{.Names}}')" ]; then
+        echo -e "\e[1;32mSuccess: MongoDB container is already running\e[0m"
+    
+    else
+        #* If MongoDB container is not running, start it
+        echo -e "\e[1;33mWARNING: MongoDB container is not running\e[0m"
+        echo -e "\e[1;34mStarting MongoDB container...\e[0m"
+        #* Start MongoDB container and exit if it fails
+        if ! sudo docker start mongodb > /dev/null; then
+            echo -e "\e[1;31mError: Failed to start MongoDB container\e[0m" >&2
+            exit 1
+        fi
+        echo -e "\e[1;32mSuccess: MongoDB container started\e[0m"
+    fi
+else
+    echo -e "\e[1;31mError: MongoDB container does not exist\e[0m"
+    echo -e "\e[1;34mCreating MongoDB container...\e[0m"
+    #* Pull MongoDB container and exit if it fails
+    if ! sudo docker pull mongo:latest; then
+        echo -e "\e[1;31mError: Failed to pull MongoDB container\e[0m" >&2
+        exit 1
+    fi
+    echo -e "\e[1;32mSuccess: MongoDB container pulled\e[0m"
+    #* Create MongoDB container
+    echo -e "\e[1;34mCreating MongoDB container...\e[0m"
     sudo docker run -d --name mongodb \
         -v $(pwd)/data/db:/data/db \
         -p 27017:27017 \
-        mongo:latest
+        mongo:latest    
+    echo -e "\e[1;34mWaiting for MongoDB to start...\e[0m"
+    sleep 10
+    echo -e "\e[1;32mSuccess: MongoDB container created\e[0m"    
 fi
 
-# Wait for MongoDB to start
-echo "Waiting for MongoDB to start..."
-sleep 10
-
-
-# Get and copy the latest file
+#* Get and copy the latest file
+sudo chown -R $USER:$USER ~/Code/CVE_DB/vuln-db-backend/data/db && sudo chmod -R 755 ~/Code/CVE_DB/vuln-db-backend/data/db
 LATEST_FILE=$(gsutil ls gs://cve-storage-bucket/enriched-cves-*.json | sort -r | head -n1)
+echo -e "\e[1;34mGetting the latest file...\e[0m"
 gsutil -q cp $LATEST_FILE ~/Code/CVE_DB/vuln-db-backend/data/db/enriched-cves-latest.json
+echo -e "\e[1;32mSuccess: Latest file copied\e[0m"
 
-# Get and copy the first (oldest) file
-FIRST_FILE=$(gsutil ls gs://cve-storage-bucket/enriched-cves-*.json | sort -r | tail -n1)
-gsutil -q cp $FIRST_FILE ~/Code/CVE_DB/vuln-db-backend/data/db/enriched-cves-first.json
+#* Check if the file exists in the VM and use the example file if it doesn't
 
-# Check if the file exists
-if [ -f ~/Code/CVE_DB/vuln-db-backend/data/db/enriched-cves-latest.json ]; then
-    echo "File exists"
-    # Update MongoDB with the latest CVEs
-    sudo docker exec mongodb mongoimport --jsonArray --db vuln_db --collection cves --drop --file /data/db/enriched-cves-latest.json
-    echo "$(date): Updated CVE files and MongoDB database" >> ~/Code/CVE_DB/vuln-db-backend/data/db/update.log
+echo -e "\e[1;34mChecking if the file exists... in the VM\e[0m"
+if [ ! -f ~/Code/CVE_DB/vuln-db-backend/data/db/enriched-cves-latest.json ]; then
+    echo -e "\e[1;33mWARNING: Latest file does not exist in the VM\e[0m"
+    if [ -f ~/Code/CVE_DB/vuln-db-backend/data/db/enriched-cves-example.json ]; then
+        echo -e "\e[1;34mUsing example file...\e[0m"
+        echo -e "\e[1;34mCopying example file to the container...\e[0m"
+        sudo docker cp ~/Code/CVE_DB/vuln-db-backend/data/db/enriched-cves-example.json mongodb:/data/db/enriched-cves-example.json
+        echo -e "\e[1;32mSuccess: Example file copied to the container\e[0m"
+        #* Update MongoDB with the example file
+        echo -e "\e[1;34mUpdating MongoDB with the example file...\e[0m"
+        sudo docker exec mongodb mongoimport --jsonArray --db vuln_db --collection cves --drop --file /data/db/enriched-cves-example.json
+        echo -e "\e[1;32mSuccess: MongoDB updated with the example file\e[0m"   
+    else
+        echo -e "\e[1;31mError: Example file does not exist\e[0m"
+        exit 1
+    fi
 else
-    echo "File does not exist. Importing example file"
-    sudo docker exec mongodb mongoimport --jsonArray --db vuln_db --collection cves --drop --file /data/db/enriched-cves-example.json
+    echo -e "\e[1;32mSuccess: File exists\e[0m"
+    echo -e "\e[1;34mCopying the file to the container...\e[0m"
+    sudo docker cp ~/Code/CVE_DB/vuln-db-backend/data/db/enriched-cves-latest.json mongodb:/data/db/enriched-cves-latest.json
+    echo -e "\e[1;32mSuccess: File copied to the container\e[0m"
+    #* Update MongoDB with the latest CVEs
+    echo -e "\e[1;34mUpdating MongoDB with the latest CVEs...\e[0m"
+    sudo docker exec mongodb mongoimport --jsonArray --db vuln_db --collection cves --drop --file /data/db/enriched-cves-latest.json
+    echo -e "\e[1;32mSuccess: MongoDB updated with the latest CVEs\e[0m"
 fi
